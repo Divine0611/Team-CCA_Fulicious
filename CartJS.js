5d7238e8ac2038e13b1ebefa93a6b188d739e019
Divine0611 <72679642+Divine0611@users.noreply.github.com>
2020-11-14 17:06:20 +0530

    Add files via upload

---
+++ CartJS.js
@@ -0,0 +1,114 @@
+var cost=0;
+       function cake1(){
+              var txtVal = document.getElementById('atc').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+299;  
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;
+       }
+       function cake2(){
+              var txtVal = document.getElementById('atc2').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+399;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake3(){
+              var txtVal = document.getElementById('atc3').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+299;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake4(){
+              var txtVal = document.getElementById('atc4').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+99;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake5(){
+              var txtVal = document.getElementById('atc5').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+99;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake6(){
+              var txtVal = document.getElementById('atc6').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+199;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake7(){
+              var txtVal = document.getElementById('atc7').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+99;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake8(){
+              var txtVal = document.getElementById('atc8').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+299;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake9(){
+              var txtVal = document.getElementById('atc9').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+99;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake10(){
+              var txtVal = document.getElementById('atc10').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+99;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function cake11(){
+              var txtVal = document.getElementById('atc11').value,
+                    listNode = document.getElementById('list'),
+                    liNode = document.createElement("LI"),
+                    txtNode = document.createTextNode(txtVal);
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+                 cost=cost+99;
+                 document.getElementById("p100").innerHTML="Total= ₹"+cost;  
+       }
+       function buy(){
+                 alert("We have received your order");
+       }
